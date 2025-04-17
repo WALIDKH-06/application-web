a5d5b61437e3597511ed8a4f3cca80f5106af3c9
WALIDKH-06 <khaoudi.walid8725.fsts@uhp.ac.ma>
2025-04-17 03:12:15 +0100

    Ton message de commit

--- f-auth/app.py
+++ f-auth/app.py
@@ -9,6 +9,7 @@ from numpy import log as Ln, exp as e
 import re
 
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
